506ea1c24ad01da3602a7a90f8673ac7aa21eac3
Timothy SanderS <moonbot@moonbot.local>
2015-04-28 17:41:58 -0400

    made network init script reset the portproxy on each run

--- proxybotnetworkinit.cmd
+++ proxybotnetworkinit.cmd
@@ -1,14 +1,16 @@
 @echo off
 
-SET IP=172.16.27.202
+SET IP=172.16.29.235
 rem echo %IP%
 
 echo "Adding ip alias to catch traffic 192.168.1.106:"
 netsh interface ip add address "Local Area Connection" 192.168.1.106 255.0.0.0
 
-echo "removing existing portproxies"
+echo "reseting any existing portproxies"
 netsh interface portproxy reset
 
 echo "setting up port proxy to outside network %IP%"
+rem netsh interface portproxy add v4tov4 listenport=12345 connectaddress=10.0.7.216 connectport=12345 listenaddress=192.168.1.106 protocol=tcp
+
 netsh interface portproxy add v4tov4 listenport=12345 connectport=12345 connectaddress=%IP%
 
